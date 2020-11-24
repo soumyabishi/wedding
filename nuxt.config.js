75c5aff07b903fcf48157752dbaa7118ea274e1e
soumyabishi <soumyabishi@outlook.com>
2020-11-24 22:26:37 +0530

    Added global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@/assets/styles/app'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
